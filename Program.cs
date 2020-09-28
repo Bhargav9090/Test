524c0110124e0e82a20ed920f7ebd480ca216732
Bhargav9090 <72032651+Bhargav9090@users.noreply.github.com>
2020-09-29 00:16:13 +0530

    my first cmmit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.WriteLine("Input the string : ");
+            str = Console.ReadLine().ToLower();
+
+            var res = str.GroupBy(p => p).Select(p => new { Count = p.Count(), Char = p.Key }).GroupBy(p => p.Count, p => p.Char).OrderByDescending(p => p.Key).First();
+
+
+            foreach (var r in res)
+            {
+                Console.WriteLine("Frequent character : '{0}', Count : {1} \n\n", r, res.Key);
+                return;
+            }
+        }
+    }
+}
